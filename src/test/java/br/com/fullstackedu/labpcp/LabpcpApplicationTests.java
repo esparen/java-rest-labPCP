bd8a6f2a9e2015cd56d3a77d98e0f266300d5e80
Eder Sparenberger <edsparen@gmail.com>
2024-04-20 10:22:51 -0300

    Feat(java-spring): Initial commit with Maven and Spring Boot settings

---
+++ src/test/java/br/com/fullstackedu/labpcp/LabpcpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstackedu.labpcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabpcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
