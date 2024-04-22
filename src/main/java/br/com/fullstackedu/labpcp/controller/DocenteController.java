f3534b06d196138fa551d920ec2c0272dbaf15e5
Eder Sparenberger <edsparen@gmail.com>
2024-04-22 10:28:31 -0300

    feat(Docente): adds DELETE route and methods to remove a Docente Entity.

--- src/main/java/br/com/fullstackedu/labpcp/controller/DocenteController.java
+++ src/main/java/br/com/fullstackedu/labpcp/controller/DocenteController.java
@@ -5,7 +5,9 @@ import br.com.fullstackedu.labpcp.controller.dto.request.DocenteUpdateRequest;
 import br.com.fullstackedu.labpcp.controller.dto.response.NovoDocenteResponse;
 import br.com.fullstackedu.labpcp.service.DocenteService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -71,14 +73,34 @@ public class DocenteController {
             @PathVariable Long docenteId,
             @Valid @RequestBody DocenteUpdateRequest docenteUpdateRequest,
             @RequestHeader(name = "Authorization") String authToken) {
-        log.info("PUT /docentes {}", docenteUpdateRequest);
+        log.info("PUT /docentes");
         String actualToken = authToken.substring(7);
         NovoDocenteResponse response = docenteService.updateDocente(docenteId , docenteUpdateRequest, actualToken);
         if (response.success()) {
             log.info("PUT /docentes -> OK ");
         } else {
-            log.error("GET /docentes -> 400");
+            log.error("PUT /docentes -> 400");
         }
         return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+
+    @DeleteMapping("/{docenteId}")
+    public ResponseEntity<NovoDocenteResponse> deleteDocente(
+            @PathVariable @NotNull(message = "ID de Docente é requerido para excluão") Long docenteId,
+            @RequestHeader(name = "Authorization") String authToken) {
+        log.info("DELETE /docentes");
+        String actualToken = authToken.substring(7);
+        NovoDocenteResponse response = docenteService.deleteDocente(docenteId, actualToken);
+        if (response.success()) {
+            log.info("DELETE /docentes -> OK ");
+        } else {
+            log.error("DELETE /docentes -> 400");
+        }
+        return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteAllDocentes(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Rota DELETE disponível apenas para registos individuais. Ex: /docentes/ID");
     }
 }
