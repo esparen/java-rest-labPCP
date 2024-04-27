efff743d3e707d14db8aa70d5757be91bc0464a1
Eder Sparenberger <edsparen@gmail.com>
2024-04-27 14:43:37 -0300

    feat(cursos): adds GET by id route, methods, and validation/ Refactors perfil validation

--- src/main/java/br/com/fullstackedu/labpcp/service/CursoService.java
+++ src/main/java/br/com/fullstackedu/labpcp/service/CursoService.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Slf4j
@@ -21,12 +22,16 @@ public class CursoService {
     private final CursoRepository cursoRepository;
     private final LoginService loginService;
 
+    private boolean _isAuthorized(String actualToken) {
+        String papelName =  loginService.getFieldInToken(actualToken, "scope");
+        List<String> authorizedPapeis =  Arrays.asList("ADM", "PEDAGOGICO");
+        return authorizedPapeis.contains(papelName);
+    }
+
     public CursoResponse insertCurso(CursoRequest cursoRequest, String actualToken) {
         try {
-            String papelName =  loginService.getFieldInToken(actualToken, "scope");
-            List<String> authorizedPapeis =  Arrays.asList("ADM", "PEDAGOGICO");
-            if (!authorizedPapeis.contains(papelName)){
-                String errMessage = "Usuários com papel [" + papelName + "] não tem acesso a essa funcionalidade";
+            if (!_isAuthorized(actualToken)){
+                String errMessage = "Usuários logado não tem acesso a essa funcionalidade";
                 log.error(errMessage);
                 return new CursoResponse(false, LocalDateTime.now() , errMessage , null, HttpStatus.UNAUTHORIZED);
             }
@@ -43,5 +48,23 @@ public class CursoService {
         CursoEntity insertedCurso = cursoRepository.save(newCurso);
         log.info("Curso adicionada com sucesso: {}", newCurso.getId());
         return new CursoResponse(true, LocalDateTime.now(),"Curso adicionada com sucesso.", Collections.singletonList(insertedCurso), HttpStatus.CREATED);
+    }
+
+    public CursoResponse getById(Long cursoId, String actualToken) {
+        try {
+            if (!_isAuthorized(actualToken)){
+                String errMessage = "Usuário logado não tem acesso a essa funcionalidade";
+                log.error(errMessage);
+                return new CursoResponse(false, LocalDateTime.now() , errMessage , null, HttpStatus.UNAUTHORIZED);
+            }
+            CursoEntity targetCurso = cursoRepository.findById(cursoId).orElse(null);
+            if (Objects.isNull(targetCurso)){
+                return new CursoResponse(false, LocalDateTime.now() , "Curso ID "+cursoId+" não encontrado." , null, HttpStatus.NOT_FOUND);
+            } else
+                return new CursoResponse(true, LocalDateTime.now() , "Curso encontrado" , Collections.singletonList(targetCurso), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Falha ao buscar Curso ID {}. Erro: {}", cursoId, e.getMessage());
+            return new CursoResponse(false, LocalDateTime.now() , e.getMessage() , null, HttpStatus.BAD_REQUEST );
+        }
     }
 }
