1b37a0e8017fd40082f72e44d8ff9fbb7251ce1f
Eder Sparenberger <edsparen@gmail.com>
2024-04-28 21:34:50 -0300

    feat(notas): adds methods query by NotaId alongside AlunoId

--- src/main/java/br/com/fullstackedu/labpcp/database/repository/NotaRepository.java
+++ src/main/java/br/com/fullstackedu/labpcp/database/repository/NotaRepository.java
@@ -4,7 +4,10 @@ import br.com.fullstackedu.labpcp.database.entity.NotaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotaRepository extends JpaRepository<NotaEntity, Long> {
     List<NotaEntity> findByAlunoId(Long alunoId);
+
+    Optional<NotaEntity> findByIdAndAlunoId(Long notaId, Long AlunoId);
 }
