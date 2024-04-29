9e09890e7e9cbd15e779647bb35bfd2076b85763
Eder Sparenberger <edsparen@gmail.com>
2024-04-28 21:31:27 -0300

    fix(Nota): adds methods to simply responses of the route

--- src/main/java/br/com/fullstackedu/labpcp/controller/dto/response/NotaResponse.java
+++ src/main/java/br/com/fullstackedu/labpcp/controller/dto/response/NotaResponse.java
@@ -8,4 +8,24 @@ import java.util.List;
 
 public record NotaResponse(Boolean success, LocalDateTime timestamp, String message, List<NotaEntity> notaData, HttpStatus httpStatus) {
 
+    public static NotaResponse createErrorResponse(HttpStatus status, String message) {
+            return new NotaResponse(
+                    false,
+                    LocalDateTime.now(),
+                    message,
+                    null,
+                    status
+            );
+    }
+
+    public static NotaResponse createSuccessResponse(HttpStatus status, String message, List<NotaEntity> notaData) {
+        return new NotaResponse(
+                true,
+                LocalDateTime.now(),
+                message,
+                notaData,
+                status
+        );
+    }
+
 }
